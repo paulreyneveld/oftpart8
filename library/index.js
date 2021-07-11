bf58e46ade751c9f6bff153619370068f04a9652
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-07-10 20:17:39 -0700

    Added support for an allBooks query, sans the genres' array

--- library/index.js
+++ library/index.js
@@ -84,16 +84,26 @@ let books = [
 ]
 
 const typeDefs = gql`
+
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    id: ID!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: () => books
   }
 }
 
