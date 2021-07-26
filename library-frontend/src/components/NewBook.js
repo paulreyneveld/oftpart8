70ae47c6656756266835e27d733a39fc1b7d80f7
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-07-25 18:40:51 -0700

    Finished 8.10

--- library-frontend/src/components/NewBook.js
+++ library-frontend/src/components/NewBook.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useMutation } from '@apollo/client'
-import { NEW_BOOK } from '../queries'
+import { NEW_BOOK, ALL_BOOKS, ALL_AUTHORS } from '../queries'
 
 const NewBook = (props) => {
   const [title, setTitle] = useState('')
@@ -9,7 +9,9 @@ const NewBook = (props) => {
   const [genre, setGenre] = useState('')
   const [genres, setGenres] = useState([])
 
-  const [ newBook ] = useMutation(NEW_BOOK)
+  const [ newBook ] = useMutation(NEW_BOOK, {
+    refetchQueries: [ { query: ALL_BOOKS }, { query: ALL_AUTHORS } ]
+  })
 
   if (!props.show) {
     return null
