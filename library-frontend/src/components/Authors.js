a89e1048d72b70709b2ccebfd948cace3ed40924
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-07-27 18:34:57 -0700

    Added a form to the Authors component

--- library-frontend/src/components/Authors.js
+++ library-frontend/src/components/Authors.js
@@ -1,12 +1,21 @@
   
-import React from 'react'
+import React, { useState } from 'react'
 
 const Authors = (props) => {
+
+  const [name, setName] = useState()
+  const [born, setBorn] = useState()
+
   if (!props.show) {
     return null
   }
 
   const authors = props.authors
+
+  const submit = (event) => {
+    event.preventDefault()
+    console.log('submit')
+  }
 
   return (
     <div>
@@ -31,7 +40,17 @@ const Authors = (props) => {
           )}
         </tbody>
       </table>
-
+      <h2>Set birthyear</h2>
+      <form onSubmit={submit}>
+        <div>
+          title
+          <input
+            value={name}
+            onChange={({ target }) => setName(target.value)}
+          />
+        </div>
+        <button type='submit'>create book</button>
+      </form>
     </div>
   )
 }
