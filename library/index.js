181a571247c51d48e4964256d4132d2a1184c7f0
Paul <63985305+paulreyneveld@users.noreply.github.com>
2021-08-04 14:55:42 -0700

    Added ability to MongoDB

--- library/index.js
+++ library/index.js
@@ -1,5 +1,20 @@
 const { ApolloServer, gql } = require('apollo-server');
-const { v1: uuid} = require('uuid');
+const mongoose = require('mongoose')
+const Author = require('./models/author')
+const Book = require('./models/book')
+
+const MONGODB_URI = 'mongodb+srv://fullstack:halfstack@cluster0-ostce.mongodb.net/graphql?retryWrites=true'
+
+console.log('connecting to', MONGODB_URI)
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connection to MongoDB:', error.message)
+  })
+
 
 let authors = [
   {
